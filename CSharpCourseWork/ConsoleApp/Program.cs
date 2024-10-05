c27e461579744878e2f04c71aa010a982bac6119
Tanel Paal <tanelpaal@gmail.com>
2024-10-06 01:17:58 +0300

    Catchup 1

--- CSharpCourseWork/ConsoleApp/Program.cs
+++ CSharpCourseWork/ConsoleApp/Program.cs
@@ -1,5 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MenuSystem;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Setup on Laptop");
-Console.WriteLine("Setup on Desktop");
+var mainMenu = new Menu("TIC-TAC-TWO", [
+    new MenuItem()
+    {
+        Shortcut = "O",
+        Title = "Options",
+    },
+    new MenuItem()
+    {
+        Shortcut = "N",
+        Title = "New Game",
+    }
+]);
+
+mainMenu.Run();
+
+return;
+// ========================================
+
+static void MenuMain()
+{
+    MenuStart();
+    
+    Console.WriteLine("O) Options");
+    Console.WriteLine("N) New Game");
+    Console.WriteLine("L) Load Game");
+    Console.WriteLine("Q) Exit");
+    
+    MenuEnd();
+}
+
+void MenuOptions()
+{
+    MenuStart();
+    
+    Console.WriteLine("Choose symbol for player one (X)");
+    Console.WriteLine("Choose symbol for player one (O)");
+    
+    MenuEnd();
+}
+
+static void MenuEnd()
+{
+    Console.WriteLine();
+    Console.Write(">");
+}
+
+static void MenuStart()
+{
+    Console.Clear();
+    Console.WriteLine("TIC-TAC-TWO");
+    Console.WriteLine("==================");
+}
---
+++ CSharpCourseWork/MenuSystem/Menu.cs
@@ -0,0 +1,80 @@
+﻿namespace MenuSystem;
+
+public class Menu
+{
+    private string MenuHeader { get; set; }
+    private static string _menuDivider = "====================";
+    private List<MenuItem> MenuItems { get; set; }
+    
+    public Menu(string menuHeader, List<MenuItem> menuItems)
+    {
+        if (string.IsNullOrWhiteSpace(menuHeader))
+        {
+            throw new ApplicationException("Menu header cannot be null or empty");
+        }
+        
+        MenuHeader = menuHeader;
+        
+        if (menuItems == null || menuItems.Count == 0)
+        {
+            throw new ApplicationException("Menu items cannot be null or empty");
+        }
+        
+        MenuItems = menuItems;
+    }
+
+    public void Run()
+    {
+        Console.Clear();
+
+        var userInput = "";
+        do
+        {
+            DrawMenu();
+            
+            userInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Be nice!!! Choose actually something!!! Try again....Oh GOD...");
+                Console.WriteLine();
+            }
+            else
+            {
+                userInput = userInput.ToUpper();
+
+                var userInputOk = false;
+
+                foreach (var menuItem in MenuItems)
+                {
+                    if (menuItem.Shortcut.ToUpper() != userInput) continue;
+
+                    userInputOk = true;
+                    break;
+                }
+
+                if (userInputOk == false)
+                {
+                    userInput = "";
+                    Console.WriteLine("For the love of God choose something from the existing options...A-10 taking off!");
+                    Console.WriteLine();
+                }
+            }
+            
+        } while (string.IsNullOrWhiteSpace(userInput));
+    }
+
+    private void DrawMenu()
+    {
+        Console.WriteLine(MenuHeader);
+        Console.WriteLine(_menuDivider);
+        
+        foreach (var t in MenuItems)
+        {
+            Console.WriteLine(t);
+        }
+
+        Console.WriteLine();
+
+        Console.Write(">");
+    }
+}
