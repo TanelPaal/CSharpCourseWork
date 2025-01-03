05dd36f006687e3b7d5d40c44211769249ab6920
tanpaa <tanpaa@taltech.ee>
2025-01-03 13:16:22 +0200

    Removed some uneccesary code.

--- CSharpCourseWork/ConsoleApp/Menus.cs
+++ CSharpCourseWork/ConsoleApp/Menus.cs
@@ -16,52 +16,14 @@ public class Menus
         ]
     );
     
-    public static readonly Menu OptionsMenu =
-        new Menu(
-            EMenuLevel.Secondary,
-            "TIC-TAC-TWO Options", [
-                new MenuItem()
-                {
-                    Shortcut = "X",
-                    Title = "X Starts",
-                    MenuItemAction = DeepMenu.Run
-                },
-                new MenuItem()
-                {
-                    Shortcut = "O",
-                    Title = "O Starts",
-                    MenuItemAction = DummyMethod
-                }
-            
-            ]);
-    
     public Menu MainMenu = new Menu(
         EMenuLevel.Main,
         "TIC-TAC-TWO", [
-            /*new MenuItem()
-            {
-                Shortcut = "O",
-                Title = "Options",
-                MenuItemAction = OptionsMenu.Run
-            },*/
             new MenuItem()
             {
                 Shortcut = "S",
                 Title = "Start",
                 MenuItemAction = GameController.MainLoop  // Remove () at the end of MainLoop for it to work.
-            },
-            /*new MenuItem()
-            {
-                Shortcut = "L",
-                Title = "Load game",
-                MenuItemAction = SavedGamesLoader.MainLoop
-            }*/
+            }
         ]);
-    
-    private static string DummyMethod()
-    {
-        Console.Write("Just press any key to get out from here! (Any key - as a random choice from keyboard....)");
-        Console.ReadKey();
-        return "foobar";
-    }
 }
